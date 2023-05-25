9888e4a69f8526954b6253aa601bc2ee7591529e
Tyrabims <abimbola.anibaba@yahoo.com>
2023-05-25 23:58:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+//import 'bootstrap/dist/css/bootstrap.css'
+import { TodoForm } from './components/TodoForm'
+//import { Task } from "./Task";
+
+
+function App() {
+
+
+}
+export default App
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+export const TodoForm = () => {
+  return (
+    
+       
+    
+  )
+}
